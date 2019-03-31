39cdfc409d222685759555a61d81e0122d0b4074
Chris Chuang <chrischuang0421@gmail.com>
2019-04-01 00:54:49 +0800

    Refine preview image size

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -2,23 +2,29 @@ import LightBox from './LightBox';
 
 const imgFolderPath = './assets/images/';
 const imgNames = [
-  'fox-01.jpg',
   'fox-02.jpg',
-  'fox-03.jpg',
   'fox-04.jpg',
+  'fox-01.jpg',
+  'fox-06.jpg',
   'fox-05.jpg',
-  'fox-06.jpg'
+  'fox-03.jpg'
 ];
-const imgPaths = imgNames.map(imgName => imgFolderPath + imgName);
+const imgSmallPaths = imgNames.map(
+  imgName => imgFolderPath + imgName.split('.').join('-s.')
+);
+const imgBigPaths = imgNames.map(imgName => imgFolderPath + imgName);
+console.log(imgSmallPaths);
 
 class App {
   constructor() {
-    this.lightBox = new LightBox(imgPaths);
     this.initPreviewBox();
+    console.log('A');
+    this.lightBox = new LightBox(imgBigPaths);
+    console.log('B');
   }
   initPreviewBox() {
     const boxArea = document.querySelector('.img-box-area');
-    imgPaths.forEach((imgPath, idx) => {
+    imgSmallPaths.forEach((imgPath, idx) => {
       const imgBox = document.createElement('div');
       imgBox.className = 'img-box';
       imgBox.addEventListener('click', () => {
